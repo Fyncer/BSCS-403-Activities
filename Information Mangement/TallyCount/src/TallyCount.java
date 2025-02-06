4dee3248f40fa8cec1ba4cde49960363f939d54a
Ren <Libramonteren1394@gmail.com>
2025-02-06 17:45:37 +0800

    add scanner to TallyCount

--- Information Mangement/TallyCount/src/TallyCount.java
+++ Information Mangement/TallyCount/src/TallyCount.java
@@ -1,7 +1,17 @@
+import java.util.*;
 public class TallyCount {
     public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+
         while(true) {
-            System.out.println("[1] ");
+            try {
+                System.out.println("[1] ");
+                System.out.println("[2] ");
+                
+            }
+            catch ( InputMismatchException e ) {
+
+            }
         }
     }
 }
