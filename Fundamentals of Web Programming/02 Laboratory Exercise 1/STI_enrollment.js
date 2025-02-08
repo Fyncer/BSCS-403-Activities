773c9b768b1a9636db26ad8d4c2c779195abfc67
Ren <Libramonteren1394@gmail.com>
2025-02-08 22:16:35 +0800

    upscale thumbnail, add parallax effect, add grid display to text inputs, add background colors to input when in focus

--- Fundamentals of Web Programming/02 Laboratory Exercise 1/STI_enrollment.js
+++ Fundamentals of Web Programming/02 Laboratory Exercise 1/STI_enrollment.js
@@ -3,14 +3,30 @@ var viewer = document.getElementById("viewer");
 
 document.getElementById("playbutton").onclick = function() {
     document.body.classList.add("remove-scrolling"); 
-    viewer.style.display = "flex";
-    body.scroll
+    viewer.style.display = "flex"; 
+   
+    setTimeout(() => {
+        viewer.classList.add("show");
+    }, 50);
 }
 
 document.getElementById("closeViewer").onclick = function() {
     document.body.classList.remove("remove-scrolling");     
-    viewer.style.display = "none";
+
+    viewer.classList.remove("show");
+    viewer.classList.add("hide");
+
+    setTimeout(() => {
+        viewer.style.display = "none";
+        viewer.classList.remove("hide")
+    }, 500);
 
     video.pause();
     video.currentTime = 0;
 }
+
+window.addEventListener("scroll", function() {
+    let thumbnail = document.getElementById("thumbnail");
+    let scrollPosition = window.scrollY;
+    thumbnail.style.backgroundPositionY = `${scrollPosition * 0.3}px`;
+});
