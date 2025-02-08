bd8d9a5ab330b41bf2b8eae4ac106d9ef7b9a035
Ren <Libramonteren1394@gmail.com>
2025-02-08 13:34:38 +0800

    add header section, add comments to css, revise redundant css code

--- Fundamentals of Web Programming/02 Laboratory Exercise 1/STI_enrollment.js
+++ Fundamentals of Web Programming/02 Laboratory Exercise 1/STI_enrollment.js
@@ -2,10 +2,13 @@ var video = document.getElementById("enrollmentVid");
 var viewer = document.getElementById("viewer");
 
 document.getElementById("playbutton").onclick = function() {
+    document.body.classList.add("remove-scrolling"); 
     viewer.style.display = "flex";
+    body.scroll
 }
 
 document.getElementById("closeViewer").onclick = function() {
+    document.body.classList.remove("remove-scrolling");     
     viewer.style.display = "none";
 
     video.pause();
